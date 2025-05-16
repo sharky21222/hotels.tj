a11bc6d84f8753b75a67a9086743a71b500cf337
sharky21222 <amir.dilshodobich@gmail.com>
2025-05-16 20:29:21 +0500

    fix: tailwind purge content for production

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 // src/index.js
+import './index.css';  // можно оставить по необходимости 
+import './App.css';  
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-import './index.css';  // можно оставить по необходимости
-import './App.css';    // глобальные стили приложения
+// можно оставить по необходимости    // глобальные стили приложения
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
